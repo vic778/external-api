098b1b6d33e9d0bb714a114f460c3648dcf0c549
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-02 13:08:15 +0300

    Crud

---
+++ app/controllers/cuisines_controller.rb
@@ -0,0 +1,55 @@
+require 'dotenv'
+require 'json'
+Dotenv.load
+
+class CuisinesController < ApplicationController 
+   skip_before_action :verify_authenticity_token
+
+   @@URL = "https://jummia.herokuapp.com/api/categories"
+
+   def index
+      @response =  RestClient.get @@URL,
+         {content_type: :json, accept: :json, "user-key": ENV["API_KEY"]}
+         @response = JSON.parse(@response)
+
+         # @response = JSON.parse(@response.body)["categories"]
+   
+      #  @city_info = JSON.parse(@response.body)["location_suggestions"][0]
+      
+       if @response
+      #  @cuisines = RestClient.get "https://jummia.herokuapp.com/api/categories/=#{@city_info["id"]}", 
+      #    {content_type: :json, accept: :json, "user-key": ENV["API_KEY"]}
+
+      #  @city_info["cuisines"] = JSON.parse(@cuisines.body)["cuisines"]
+
+         render json: @response
+      else
+         render json: {message: "City Not Found", error: 404}
+      end
+  end
+
+   def show
+      @response =  RestClient.get "https://jummia.herokuapp.com/api/categories/#{params[:id]}",
+         {content_type: :json, accept: :json, "user-key": ENV["API_KEY"]}
+         @response = JSON.parse(@response)
+
+      if @response
+
+         render json: @response
+      else
+         render json: {message: "City Not Found", error: 404}
+      end
+  end
+
+   def create
+      @response =  RestClient.post @@URL,
+         {content_type: :json, accept: :json, "user-key": ENV["API_KEY"]}   
+         @response = JSON.parse(@response)  
+      if @response
+         render json: @response
+      else
+         render json: {message: "City Not Found", error: 404}
+      end
+  end
+
+end 
